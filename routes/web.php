4a52d5834e3d67ddf300c57ecc0ccf130237a6f4
abdellah <admirablea42@gmail.com>
2024-05-26 21:17:45 +0200

    simple change

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('post', PostController::class);
 
+Route::get('/', function () {
+    return to_route('post.index');
+});
+
 Route::get('all-posts', function () {
     $posts = Post::all();
     return view('all-posts', compact('posts'));
